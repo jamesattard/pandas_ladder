461cf827c9425817c257649939812f043f2c4ced
James J Attard <info@jamesattard.com>
2017-09-01 10:43:49 +0200

    segregation of duties methods

--- ladder.py
+++ ladder.py
@@ -3,7 +3,7 @@ from dateutil.relativedelta import relativedelta
 import numpy as np
 import pandas as pd
 
-def ladder(df, start_date, date_bins, date_bins_labels):
+def generate_df(df, start_date, date_bins, date_bins_labels):
     """
     Build a ladder based on the provided timeseries bins.
     
@@ -12,7 +12,6 @@ def ladder(df, start_date, date_bins, date_bins_labels):
     start_date          -- starting date
     date_bins           -- date bins that take the start_date as delta reference
     date_bins_labels    -- labels for the date bins
-    group_by            -- how the aggregate df will be grouped
     """
 
     bins = [start_date,]
@@ -26,10 +25,20 @@ def ladder(df, start_date, date_bins, date_bins_labels):
         bins.append(new_date)
 
     df['categories'] = pd.cut(df.index, list(map(datetup, bins)), labels=date_bins_labels)
+
+    return df
+
+def timeseries_binning(df):
     dx = df.groupby(['categories']).sum().transpose()
-    dy = df.groupby(['colors', 'categories']).sum().transpose()
+    return dx
+
+def group_df(df, group_by):
+    """
+    df          -- dataframe to process
+    group_by    -- how the aggregate df will be grouped
+    """
+    dy = df.groupby([group_by, 'categories']).sum().transpose()
     dz = dy.stack(0).reset_index(level=0, drop=True).fillna(0).astype(int)
-    #return df, dx, dy, dz
     return dz
 
 data = {
@@ -42,5 +51,11 @@ date_now = date.today()
 bins = [6,12,999]
 labels = ['Alpha', 'Beta', 'Omega']
 
-lad = ladder(data, date_now, bins, labels)
-print(lad)
+lad1 = generate_df(data, date_now, bins, labels)
+print(lad1)
+
+lad2 = timeseries_binning(lad1)
+print(lad2)
+
+lad3 = group_df(lad1, 'colors')
+print(lad3)
